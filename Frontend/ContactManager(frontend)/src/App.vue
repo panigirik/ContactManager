4ea51fff9fd0775cc530e3cf8e822f16f278b056
Zahar <zaharvaskovskij78@gmail.com>
2025-05-14 19:33:01 +0300

    update structure

---
+++ Frontend/ContactManager(frontend)/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <h1>Contact Manager</h1>
+    <ContactSearch />
+    <hr />
+    <ContactList />
+  </div>
+</template>
+
+<script setup>
+import ContactSearch from './components/ContactSearch.vue'
+import ContactList from './components/ContactList.vue'
+</script>
